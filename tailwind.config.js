b44ded5a4a3b150cc80a21dcbeb248588448e760
okwuosaoluchi <okwuosaoluchi@yahoo.com>
2024-06-02 20:07:17 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1400px',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
